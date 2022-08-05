d880f31eb4c825c29eb426d1c38d24acfcd98537
Filin-oleg <110037084+Filin-oleg@users.noreply.github.com>
2022-08-05 21:26:12 +0200

    delete last strings

--- Exam_002/Program.cs
+++ Exam_002/Program.cs
@@ -1,44 +1,46 @@
 ﻿// Задача. Написать программу, которая будет выдавать название дня недели по заданному номеру
 
-Console.Write("Введите номер дня недели ");
-int number = int.Parse(Console.ReadLine());
-if (number == 1)
+internal class Programm
 {
-    Console.WriteLine("День недели с номером 1 - это понедельник");
-}
-if (number == 2)
-{
-    Console.WriteLine("День недели с номером 2 - это вторник");
-}
-if (number == 3)
-{
-    Console.WriteLine("День недели с номером 3 - это среда");
-}
-if (number == 4)
-{
-    Console.WriteLine("День недели с номером 4 - это четверг");
-}
-if (number == 5)
-{
-    Console.WriteLine("День недели с номером 5 - это пятница");
-}
-if (number == 6)
-{
-    Console.WriteLine("День недели с номером 6 - это суббота");
-}
-if (number == 7)
-{
-    Console.WriteLine("День недели с номером 7 - это воскресенье");
-}
-if (number > 7)
-{
-    Console.WriteLine("Такого дня недели не существует");
-}
-if (number < 0)
-{
-    Console.WriteLine("Такого дня недели не существует");
-}
-if (number == 0)
-{
-    Console.WriteLine("Такого дня недели не существует");
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите номер, соответствующий дню недели ");
+        int number = int.Parse(Console.ReadLine());
+        if (number == 1)
+        {
+            Console.WriteLine("День недели с номером 1 - это понедельник");
+        }
+        if (number == 2)
+        {
+            Console.WriteLine("День недели с номером 2 - это вторник");
+        }
+        if (number == 3)
+        {
+            Console.WriteLine("День недели с номером 3 - это среда");
+        }
+        if (number == 4)
+        {
+            Console.WriteLine("День недели с номером 4 - это четверг");
+        }
+        if (number == 5)
+        {
+            Console.WriteLine("День недели с номером 5 - это пятница");
+        }
+        if (number == 6)
+        {
+            Console.WriteLine("День недели с номером 6 - это суббота");
+        }
+        if (number == 7)
+        {
+            Console.WriteLine("День недели с номером 7 - это воскресенье");
+        }
+        if (number > 7)
+        {
+            Console.WriteLine("Такого дня недели не существует");
+        }
+        if (number < 1)
+        {
+            Console.WriteLine("Такого дня недели не существует");
+        }
+    }
 }
