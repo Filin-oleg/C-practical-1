c3cfe66d2c9f75c6a8897e96b96f8b8962ff9a43
Filin-oleg <110037084+Filin-oleg@users.noreply.github.com>
2022-08-05 20:16:03 +0200

    programm done

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
-﻿// программа: Если введено 
+﻿// программа: Если введено полодительное число, то возводим его в квадрат
 
-Console.ReadLine("");
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine()); // number = Convert.ToInt32(Console.ReaDLINE());
+
+if (number > 0)
+{
+    number *= number;
+    Console.WriteLine(number);
+}
+else 
+{
+    Console.WriteLine("Число отрицательное");
+}
